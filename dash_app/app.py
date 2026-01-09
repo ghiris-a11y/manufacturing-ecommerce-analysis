c72afa492b61d2efabffbe860e02d5478ff0f464
ghiris-a11y <2025.ghiris@isu.ac.in>
2026-01-09 11:20:38 +0530

    Update app.py

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,11 +1,4 @@
-import os
-import pandas as pd
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import plotly.express as px
 
-# dash_app/app.py
 
 import os
 import pandas as pd
